991e1a433ef2f18df78d8ecb494ffe95b4b9e092
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-23 13:38:39 +0100

    Update newsLetter.js

--- pages/api/newsLetter.js
+++ pages/api/newsLetter.js
@@ -1,7 +1,15 @@
-export default function newsletterRegistration(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function newsletterRegistration(req, res) {
 	if (req.method === 'POST') {
 		const email = req.body.email;
-		console.log(email);
+		const client = await MongoClient.connect(
+			'mongodb+srv://abNextJS:JZkkU57MAyHpN3Jj@abcluster.vqjrw.mongodb.net/NextJSNewsLetter?retryWrites=true&w=majority',
+		);
+
+		const db = client.db();
+		await db.collection('emails').insertOne({ email: email });
+		client.close;
 		res.status(201).json({ message: 'Signed up!', obj: email });
 	}
 }
