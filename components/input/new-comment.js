2a89633c8b950d5f7689a22bdacb3db7e16564c1
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-23 10:42:37 +0100

    Update new-comment.js

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -36,25 +36,25 @@ function NewComment(props) {
   }
 
   return (
-    <form className={classes.form}>
-      <div className={classes.row}>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your email</label>
-          <input type='email' id='email' ref={emailInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='name'>Your name</label>
-          <input type='text' id='name' ref={nameInputRef} />
-        </div>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='comment'>Your comment</label>
-        <textarea id='comment' rows='5' ref={commentInputRef}></textarea>
-      </div>
-      {isInvalid && <p>Please enter a valid email address and comment!</p>}
-      <button>Submit</button>
-    </form>
-  );
+		<form className={classes.form} onSubmit={sendCommentHandler}>
+			<div className={classes.row}>
+				<div className={classes.control}>
+					<label htmlFor="email">Your email</label>
+					<input type="email" id="email" ref={emailInputRef} />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="name">Your name</label>
+					<input type="text" id="name" ref={nameInputRef} />
+				</div>
+			</div>
+			<div className={classes.control}>
+				<label htmlFor="comment">Your comment</label>
+				<textarea id="comment" rows="5" ref={commentInputRef}></textarea>
+			</div>
+			{isInvalid && <p>Please enter a valid email address and comment!</p>}
+			<button>Submit</button>
+		</form>
+	);
 }
 
 export default NewComment;
