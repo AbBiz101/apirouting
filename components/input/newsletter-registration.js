ea1edc9b701a225caae702d3df222b310dd3989a
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-23 09:35:59 +0100

    Update newsletter-registration.js

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,30 +1,40 @@
 import classes from './newsletter-registration.module.css';
+import { useRef } from 'react';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+	const userEmail = useRef();
+	function registrationHandler(event) {
+		event.preventDefault();
+		const enteredEmail = userEmail.current.value;
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
+		fetch('/api/newsLetter', {
+			method: 'POST',
+			body: JSON.stringify({ email: enteredEmail }),
+			headers: {
+				'content-Type': 'application/json',
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	}
 
-  return (
-    <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-          />
-          <button>Register</button>
-        </div>
-      </form>
-    </section>
-  );
+	return (
+		<section className={classes.newsletter}>
+			<h2>Sign up to stay updated!</h2>
+			<form onSubmit={registrationHandler}>
+				<div className={classes.control}>
+					<input
+						type="email"
+						id="email"
+						placeholder="Your email"
+						aria-label="Your email"
+						ref={userEmail}
+					/>
+					<button>Register</button>
+				</div>
+			</form>
+		</section>
+	);
 }
 
 export default NewsletterRegistration;
