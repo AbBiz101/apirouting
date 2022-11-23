ff9a49b95df4579b31b2d9ecbea325b33a801b64
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-23 09:35:56 +0100

    Update newsLetter.js

--- pages/api/newsLetter.js
+++ pages/api/newsLetter.js
@@ -1,3 +1,7 @@
 export default function newsletterRegistration(req, res) {
-    
+	if (req.method === 'POST') {
+		const email = req.body.email;
+		console.log(email);
+		res.status(201).json({ message: 'Signed up!', obj: email });
+	}
 }
